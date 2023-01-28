0cd53cf1840d0208e39d7d05880349f874e0fc91
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-01-29 01:32:36 +0200

    updated the UI, extracted widgets and rearranged code for readability

--- lib/pages/root.dart
+++ lib/pages/root.dart
@@ -1,3 +1,5 @@
+import 'package:call_app/models/navigation_bar_tab_item.dart';
+import 'package:call_app/resources/constants.dart';
 import 'package:call_app/resources/paths.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -11,42 +13,34 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
-  // getter that computes the current index from the current location,
-  // using the helper method below
-  int get _currentIndex => _locationToTabIndex(GoRouter.of(context).location);
-
-  int _locationToTabIndex(String location) {
-    final index = tabs(0).indexWhere((t) => location.startsWith(t.initialLocation));
-    // if index not found (-1), return 0
-    return index < 0 ? 0 : index;
-  }
-
-  // callback used to navigate to the desired tab
-  void _onItemTapped(BuildContext context, int tabIndex) {
-    if (tabIndex != _currentIndex) {
-      // go to the initial location of the selected tab (by index)
-      context.go(tabs(_currentIndex)[tabIndex].initialLocation);
-    }
-  }
+  List<NavigationBarTabItem> tabs(int i) => [
+        NavigationBarTabItem(
+          initialLocation: Paths.favorites,
+          icon: i == 0 ? const Icon(Icons.star) : const Icon(Icons.star_border),
+          label: 'Favorites',
+        ),
+        NavigationBarTabItem(
+          initialLocation: Paths.recents,
+          icon: i == 1 ? const Icon(Icons.access_time_filled_rounded) : const Icon(Icons.access_time_rounded),
+          label: 'Recents',
+        ),
+        NavigationBarTabItem(
+          initialLocation: Paths.contacts,
+          icon: i == 2 ? const Icon(Icons.people_alt) : const Icon(Icons.people_alt_outlined),
+          label: 'Contacts',
+        ),
+      ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: tabs(_currentIndex),
-        onTap: (index) => _onItemTapped(context, index),
-        selectedItemColor: Colors.black,
-        selectedFontSize: 14,
-        unselectedFontSize: 14,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-      ),
+      bottomNavigationBar: sharedBottomBar(),
       body: SafeArea(
         child: Column(
           mainAxisSize: MainAxisSize.max,
           children: [
-            topSharedBar(),
+            sharedTopBar(),
             ConstrainedBox(
               constraints: BoxConstraints(
                 maxHeight: MediaQuery.of(context).size.height - 80 - 50,
@@ -59,7 +53,31 @@ class _RootPageState extends State<RootPage> {
     );
   }
 
-  Widget topSharedBar() {
+  // getter that computes the current index from the current location
+  int get _currentIndex {
+    final index = tabs(0).indexWhere(
+      (e) => GoRouter.of(context).location.startsWith(e.initialLocation),
+    );
+    return index < 0 ? 0 : index;
+  }
+
+  Widget sharedBottomBar() {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      items: tabs(_currentIndex),
+      onTap: (index) {
+        if (index != _currentIndex) {
+          context.go(tabs(_currentIndex)[index].initialLocation);
+        }
+      },
+      selectedItemColor: Colors.black,
+      selectedFontSize: sizeH6,
+      unselectedFontSize: sizeH6,
+      selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget sharedTopBar() {
     return Padding(
       padding: const EdgeInsets.only(left: 15, right: 15),
       child: Container(
@@ -77,39 +95,10 @@ class _RootPageState extends State<RootPage> {
               child: Text('Search contacts & places'),
             ),
             Icon(Icons.keyboard_voice_outlined),
-            Icon(Icons.more_vert_outlined)
+            Icon(Icons.more_vert_outlined),
           ],
         ),
       ),
     );
   }
-
-  List<ScaffoldWithNavBarTabItem> tabs(int i) => [
-        ScaffoldWithNavBarTabItem(
-          initialLocation: Paths.favorites,
-          icon: i == 0 ? const Icon(Icons.star) : const Icon(Icons.star_border),
-          label: 'Favorites',
-        ),
-        ScaffoldWithNavBarTabItem(
-          initialLocation: Paths.recents,
-          icon: i == 1 ? const Icon(Icons.access_time_filled_rounded) : const Icon(Icons.access_time_rounded),
-          label: 'Recents',
-        ),
-        ScaffoldWithNavBarTabItem(
-          initialLocation: Paths.contacts,
-          icon: i == 2 ? const Icon(Icons.people_alt) : const Icon(Icons.people_alt_outlined),
-          label: 'Contacts',
-        ),
-      ];
-}
-
-class ScaffoldWithNavBarTabItem extends BottomNavigationBarItem {
-  const ScaffoldWithNavBarTabItem({
-    required this.initialLocation,
-    required Widget icon,
-    String? label,
-  }) : super(icon: icon, label: label);
-
-  /// The initial location/path
-  final String initialLocation;
 }
