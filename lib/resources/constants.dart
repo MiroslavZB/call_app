2d3fb282da028f795cc8076a826abc51ef6f1235
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-01-29 01:22:57 +0200

    added commonly used colors, icon sizes and font sizes to constants file

---
+++ lib/resources/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const darkAccentColor = Color.fromRGBO(77, 92, 142, 1);
+const lightAccentColor = Color.fromRGBO(222, 226, 247, 1);
+
+// Icon size
+const double regularIconSize = 25;
+const double bigIconSize = 30;
+const double extraBigIconSize = 40;
+
+// Font size
+const double sizeH1 = 26.0;
+const double sizeH2 = 24.0;
+const double sizeH3 = 22.0;
+const double sizeH4 = 18.0;
+const double sizeH5 = 16.0;
+const double sizeH6 = 14.0;
