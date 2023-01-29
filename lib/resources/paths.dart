94476cd550ea874fd010d5d13dd8814660b1167d
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-01-29 19:32:32 +0200

    added contact info page to show when a contact is opened

--- lib/resources/paths.dart
+++ lib/resources/paths.dart
@@ -3,4 +3,5 @@ abstract class Paths {
   static String get recents => '/recents';
   static String get contacts => '/contacts';
   static String get newContact => '/newContact';
+  static String get contactInfo => '/contactInfo';
 }
