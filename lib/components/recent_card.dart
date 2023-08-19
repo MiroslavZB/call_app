26976db052f41fdd9059a2436d33c06160db063e
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-08-19 15:18:32 +0300

    added countries and block for menu

--- lib/components/recent_card.dart
+++ lib/components/recent_card.dart
@@ -1,8 +1,10 @@
 import 'package:call_app/components/contact_image.dart';
+import 'package:call_app/components/icon_button_widget.dart';
 import 'package:call_app/main.dart';
 import 'package:call_app/models/contact.dart';
 import 'package:call_app/resources/constants.dart';
 import 'package:call_app/router/paths.dart';
+import 'package:call_app/state/recent_phone_menu_state.dart';
 
 Widget recentCard(
   BuildContext context, {
@@ -12,68 +14,116 @@ Widget recentCard(
   required int flag,
   required int id,
 }) {
-  return Padding(
-    padding: const EdgeInsets.only(top: 10),
-    child: Row(
-      children: [
-        TextButton(
-          onPressed: () {
-            context.pushNamed(
-              Paths.contactInfo,
-              queryParams: {'phone': phone, 'id': id.toString()},
-              extra: contact,
-            );
-          },
-          child: contactImage(
-            name: contact?.name,
-            fillColorHex: contact?.hexColor,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right: 20, left: 10),
-          child: GestureDetector(
-            onLongPress: () {
-              objectBox.recentsBox.remove(id);
-            },
+  return BlocProvider(
+    create: (_) => RecentPhoneMenuState(),
+    child: BlocBuilder<RecentPhoneMenuState, bool>(
+      builder: (context, state) => Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(20),
+          onTap: () => BlocProvider.of<RecentPhoneMenuState>(context).flip(),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: state ? lightAccentColor.withOpacity(0.1) : Colors.white,
+            ),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  contact == null ? phone : contact.name,
-                  style: const TextStyle(
-                    fontSize: sizeH3,
-                  ),
-                ),
                 Row(
                   children: [
-                    Icon(
-                      flag == 0
-                          ? Icons.call_missed
-                          : flag == 1
-                              ? Icons.call_made
-                              : Icons.call_received,
+                    TextButton(
+                      onPressed: () {
+                        context.pushNamed(
+                          Paths.contactInfo,
+                          queryParams: {'phone': phone, 'id': id.toString()},
+                          extra: contact,
+                        );
+                      },
+                      child: contactImage(
+                        name: contact?.name,
+                        fillColorHex: contact?.hexColor,
+                      ),
                     ),
-                    Text(
-                      worldTime.format(dateTime: occurrence, formatter: formatter),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20, left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              contact == null ? phone : contact.name,
+                              style: const TextStyle(
+                                fontSize: sizeH3,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  flag == 0
+                                      ? Icons.call_missed
+                                      : flag == 1
+                                          ? Icons.call_made
+                                          : Icons.call_received,
+                                ),
+                                Text(
+                                  worldTime.format(dateTime: occurrence, formatter: formatter),
+                                ),
+                              ],
+                            ),
+                            Text(countriesFromPhoneCountryCode[phone.substring(0, 2)] ??
+                                countriesFromPhoneCountryCode[phone.substring(0, 3)] ??
+                                'Unknown'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        await objectBox.addRecent(
+                          contact: contact,
+                          phone: phone,
+                          occurrence: DateTime.now(),
+                          state: 1,
+                        );
+                      },
+                      icon: const Icon(
+                        Icons.call_outlined,
+                        size: regularIconSize,
+                      ),
                     ),
                   ],
                 ),
-                const Text('Country'), // TODO
+                if (state) ...[
+                  const Divider(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      iconButtonWidget(
+                          icon: Icons.videocam_outlined,
+                          text: 'Video',
+                          onPressed: () {
+                            // TODO
+                          }),
+                      iconButtonWidget(
+                          icon: Icons.message_outlined,
+                          text: 'Text',
+                          onPressed: () {
+                            // TODO
+                          }),
+                      iconButtonWidget(
+                          icon: Icons.history,
+                          text: 'History',
+                          onPressed: () {
+                            // TODO
+                          }),
+                    ],
+                  ),
+                ],
               ],
             ),
           ),
         ),
-        Expanded(child: Container()),
-        IconButton(
-          onPressed: () async {
-            await objectBox.addRecent(contact: contact, phone: phone, occurrence: DateTime.now(), state: 1);
-          },
-          icon: const Icon(
-            Icons.call_outlined,
-            size: regularIconSize,
-          ),
-        ),
-      ],
+      ),
     ),
   );
 }
