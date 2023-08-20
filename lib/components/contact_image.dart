a56589689d66c7300f474b7957285f1da4ba37c1
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-08-20 12:21:16 +0300

    reduced code duplication

--- lib/components/contact_image.dart
+++ lib/components/contact_image.dart
@@ -11,25 +11,11 @@ Widget contactImage({
   double size = contactImageSize,
 }) {
   final Color fillColor = fillColorHex == null ? Colors.grey : Color(fillColorHex);
+  Uint8List? decodedImage;
   if (image != null) {
-    Uint8List decodedImage;
     try {
       decodedImage = base64Decode(image);
-    } catch (e) {
-      return Container(
-        height: size,
-        width: size,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: fillColor,
-        ),
-      );
-    }
-    return Image.memory(
-      decodedImage,
-      height: size,
-      width: size,
-    );
+    } catch (_) {}
   }
   return Container(
     height: size,
@@ -38,20 +24,26 @@ Widget contactImage({
       shape: BoxShape.circle,
       color: fillColor,
     ),
-    child: Center(
-      child: name == null
-          ? Icon(
-              Icons.account_circle_rounded,
-              color: Colors.grey[50],
-              size: extraBigIconSize,
-            )
-          : Text(
-              name.substring(0, 1).toUpperCase(),
-              style: styleH1.copyWith(
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
-              ),
-            ),
-    ),
+    child: decodedImage != null
+        ? Image.memory(
+            decodedImage,
+            height: size,
+            width: size,
+          )
+        : Center(
+            child: name?.isNotEmpty != true
+                ? Icon(
+                    Icons.account_circle_rounded,
+                    color: Colors.grey[50],
+                    size: extraBigIconSize,
+                  )
+                : Text(
+                    name!.substring(0, 1).toUpperCase(),
+                    style: styleH1.copyWith(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+          ),
   );
 }
